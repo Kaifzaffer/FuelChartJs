ee7a3f46bfeaeace01234ea6d54dfa9c8460f384
Kaif Zaffer <kaifzaffer748@gmail.com>
2024-09-23 09:12:43 +0530

    vite change

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
+
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactDOM from 'react-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
